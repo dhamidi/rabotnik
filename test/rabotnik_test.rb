a92500fe3f388e02939e34ac901451337028e432
Dario Hamidi <dario@gowriteco.de>
2016-08-03 02:26:44 +0300

    Add test for capturing a todo

--- test/rabotnik_test.rb
+++ test/rabotnik_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 
 class RabotnikTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Rabotnik::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  def test_rabotnik_captures_a_todo_with_some_text
+    app = Rabotnik::App.new
+    capture_todo = Rabotnik::CaptureTodo.new(text: 'write tests')
+    result = app.handle_command(capture_todo)
+    assert_nil result.errors
+    assert_equals [:todo_captured, result.events.first.name]
   end
 end
