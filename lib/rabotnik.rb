073e0a6f9a03c44d4ee00d42eff33cb233f7e79b
Dario Hamidi <dario@gowriteco.de>
2016-08-03 13:47:24 +0300

    Query all todos

--- lib/rabotnik.rb
+++ lib/rabotnik.rb
@@ -3,13 +3,38 @@ require 'securerandom'
 
 module Rabotnik
   class App
+    def initialize
+      @todos = Views::Todos.new
+    end
+
     def handle_command(command)
       event = TodoCaptured.new(todo_id: SecureRandom.uuid, text: command.text)
+      @todos.handle_event event
       Result.new([event], nil)
+    end
+
+    def query(view)
+      @todos
     end
   end
 
   Result = Struct.new(:events, :errors)
+
+  module Views
+    class Todos
+      Todo = Struct.new(:text)
+
+      attr_reader :all
+
+      def initialize
+        @all = []
+      end
+
+      def handle_event(event)
+        @all << Todo.new(event.text)
+      end
+    end
+  end
 
   class CaptureTodo
     attr_reader :text
