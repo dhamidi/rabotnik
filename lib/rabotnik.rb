14fa87bf43badc0df4601f42afc123259aaa0ad7
Dario Hamidi <dario@gowriteco.de>
2016-08-03 12:32:58 +0300

    Record captured todo text

--- lib/rabotnik.rb
+++ lib/rabotnik.rb
@@ -4,22 +4,26 @@ require 'securerandom'
 module Rabotnik
   class App
     def handle_command(command)
-      Result.new([TodoCaptured.new(todo_id: SecureRandom.uuid)], nil)
+      event = TodoCaptured.new(todo_id: SecureRandom.uuid, text: command.text)
+      Result.new([event], nil)
     end
   end
 
   Result = Struct.new(:events, :errors)
 
   class CaptureTodo
+    attr_reader :text
     def initialize(text:)
+      @text = text
     end
   end
 
   class TodoCaptured
-    attr_reader :todo_id
+    attr_reader :todo_id, :text
 
-    def initialize(todo_id:)
+    def initialize(todo_id:,text:)
       @todo_id = todo_id
+      @text = text
     end
 
     def event_name; :todo_captured; end
