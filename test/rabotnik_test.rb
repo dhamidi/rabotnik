148016b096c84e8d201dd0061ba5ad5d2ea2b493
Dario Hamidi <dario@gowriteco.de>
2016-08-03 12:30:19 +0300

    Test that capturing a todo generates an ID

--- test/rabotnik_test.rb
+++ test/rabotnik_test.rb
@@ -8,4 +8,15 @@ class RabotnikTest < Minitest::Test
     assert_nil result.errors
     assert_equal(:todo_captured, result.events.first.event_name)
   end
+
+  def test_rabotnik_capturing_a_todo_assigns_a_random_id_to_the_todo
+    app = Rabotnik::App.new
+    capture_todo = Rabotnik::CaptureTodo.new(text: 'write tests')
+    result = app.handle_command(capture_todo)
+    first_id = result.events.first.todo_id
+    result = app.handle_command(capture_todo)
+    second_id = result.events.first.todo_id
+
+    refute_equal(first_id, second_id, "todo IDs needs to differ")
+  end
 end
